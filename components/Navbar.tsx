992f16f5b8a0ab3f3f4a909f9f3d4d16ad5b909f
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-09 14:48:01 +0530

    primary google auth done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,41 +1,48 @@
-import Link from "next/link"
-import Image from "next/image"
-import { NavLinks } from "@/constant"
-import AuthProviders from "./AuthProviders"
+import Link from "next/link";
+import Image from "next/image";
+import { NavLinks } from "@/constant";
+import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {}
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
-    <nav className='flexBetween navbar'>
-      <div className='flex-1 flexStart gap-10'>
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={115} height={43}/>
+          <Image src="/logo.svg" alt="logo" width={115} height={43} />
         </Link>
 
         <ul className="xl:flex hidden text-small gap-7">
-           {NavLinks.map((link) => (
+          {NavLinks.map((link) => (
             <Link href={link.href} key={link.key}>
-             {link.text}
+              {link.text}
             </Link>
-           ))}
+          ))}
         </ul>
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-          UserPhoto
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
 
-          <Link href="/create-project">
-           Share Work
-          </Link>
+            <Link href="/create-project">Share Work</Link>
           </>
         ) : (
-         <AuthProviders/>
+          <AuthProviders />
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
